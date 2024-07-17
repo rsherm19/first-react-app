c61de056d6cd7d765878709a2086d9f0b32a4dda
Ryan Sherman <rsherman5@elon.edu>
2024-07-17 11:52:33 -0400

    learn conditional rendering and looping in React JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
-
+  const todo = { text: 'A brand new task', done: true };
+  const todos = [
+    {text: 'Learn JavaScript', done: true},
+    {text: 'Learn JSX', done: false},
+    {text: 'Learn HTML', done: true},
+    {text: 'Learn CSS', done: true},
+    {text: 'Master React', done: false},
+  ];
   return (
-    <h1>Hello, world!</h1>
+    <>
+      <h1>JavaScript in JSX</h1>
+      <p>{todo.text}</p>
+
+      <h2>Looping and Conditional Rendering</h2>
+      <p>{todo.done ? `Task Completed - ${todo.text}` : todo.text }</p>
+      <ul>
+        {todos.map((todo, index) =>
+          <li key={index}>
+            {todo.done ? `Task Completed - ${todo.text}` : todo.text}
+          </li>
+        )}
+      </ul>
+    </>
   );
 };
 
